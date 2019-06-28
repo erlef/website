7a2c8db00b0b6d42c35c33373acf5f936c12fbe8
Desmond Bowe <desmond@crevalle.io>
2019-06-28 10:26:35 -0700

    bump elixir version to 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Erlef.MixProject do
     [
       app: :erlef,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
