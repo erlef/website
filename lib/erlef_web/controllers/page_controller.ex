14aedc8229e4683a2c605c2e53a9fdc7b84ab941
Joaquin Alcerro <joaquin@nimbbo.com>
2019-07-04 00:01:37 -0600

    First approach static_map creation from files

--- lib/erlef_web/controllers/page_controller.ex
+++ lib/erlef_web/controllers/page_controller.ex
@@ -20,7 +20,40 @@ defmodule ErlefWeb.PageController do
     "/wg/" => "wg.html"
   }
 
-  def static_pages, do: @static_map
+  @base_project_directory  File.cwd!()
+  @base_map_directory  "/lib/erlef_web/templates/page/"
+
+  def create_filesystem_structure(starting_directory) do
+    directory_list = File.ls!(starting_directory)
+    Enum.map(
+      directory_list,
+      fn element ->
+        {element, File.dir?("#{starting_directory}/#{element}")}
+      end
+    )
+  end
+
+  def create_map([], _path, state), do: state
+  def create_map([{element, dir?} | _t], path, state) when dir? == true do
+    new_dir_structure = 
+      @base_project_directory <> @base_map_directory <> element
+      |> create_filesystem_structure()
+    sub_dir = path <> element <> "/"
+    create_map(new_dir_structure, sub_dir, state)
+  end
+  def create_map([{element, _dir?} | t], path, state) do
+    path_element = String.trim(element, ".html.eex")
+    html_file = String.trim(element, ".eex")
+    state = Map.merge(state, %{"#{path}#{path_element}" => html_file})
+    create_map(t, path, state)
+  end
+
+  def static_map() do
+    starting_directory = create_filesystem_structure(@base_project_directory <> @base_map_directory)
+    create_map(starting_directory, "/", %{})
+  end
+
+  def static_pages, do: static_map()
 
   def page(%{request_path: "/sponsors/"} = conn, _params) do
     sponsors = Erlef.Sponsors.roster()
