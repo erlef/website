adfca55c660ef5ffdbe3c1042ae74efae37b979b
Bryan Paxton <starbelly@pobox.com>
2019-07-22 20:47:47 -0500

    favicon and don't try to render calendar if the element is null

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -18,53 +18,54 @@ import 'bootstrap';
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
-    var defaultView = calendarEl.getAttribute("data-calendar-type") || "dayGridMonth";
+    if (calendarEl != null) { 
+        var defaultView = calendarEl.getAttribute("data-calendar-type") || "dayGridMonth";
 
-    var header;
-    if (calendarEl.getAttribute("data-calendar-header") === "true") {
-        header = {
-            left: 'prevYear,prev,next,nextYear',
-            center: 'title',
-            right: 'dayGridMonth,dayGridWeek,dayGridDay,listMonth'
-        };
-    } else { 
-        header = '';
-    }
-
-
-    var calendar = new Calendar(calendarEl, {
-        plugins: [interactionPlugin, dayGridPlugin, listPlugin, bootstrapPlugin],
-        header: header, 
-        navLinks: true,
-        themeSystem: 'bootstrap',
-        defaultView: defaultView,
-        timezone: "local",
-        eventLimit: true,
-        allDaySlot: true,
-        displayEventTime: true,
-        displayEventEnd: true,
-        firstDay: 1,
-        weekNumbers: false,
-        selectable: false,
-        weekNumberCalculation: "ISO",
-        eventLimit: true,
-        eventLimitClick: 'week', //popover
-        slotLabelFormat: 'HH:mm',
-        weekends: true,
-        nowIndicator: true,
-        dayPopoverFormat: 'dddd DD/MM',
-        events: window.calendar_events,
-        eventRender: function (info) {
-            $(info.el).popover({
-                title: info.event.title,
-                content: info.event.extendedProps.description,
-                trigger: 'hover',
-                placement: 'top',
-                container: 'body'
-            });
+        var header;
+        if (calendarEl.getAttribute("data-calendar-header") === "true") {
+            header = {
+                left: 'prevYear,prev,next,nextYear',
+                center: 'title',
+                right: 'dayGridMonth,dayGridWeek,dayGridDay,listMonth'
+            };
+        } else { 
+            header = '';
         }
-    });
 
-    calendar.render();
+        var calendar = new Calendar(calendarEl, {
+            plugins: [interactionPlugin, dayGridPlugin, listPlugin, bootstrapPlugin],
+            header: header, 
+            navLinks: true,
+            themeSystem: 'bootstrap',
+            defaultView: defaultView,
+            timezone: "local",
+            eventLimit: true,
+            allDaySlot: true,
+            displayEventTime: true,
+            displayEventEnd: true,
+            firstDay: 1,
+            weekNumbers: false,
+            selectable: false,
+            weekNumberCalculation: "ISO",
+            eventLimit: true,
+            eventLimitClick: 'week', //popover
+            slotLabelFormat: 'HH:mm',
+            weekends: true,
+            nowIndicator: true,
+            dayPopoverFormat: 'dddd DD/MM',
+            events: window.calendar_events,
+            eventRender: function (info) {
+                $(info.el).popover({
+                    title: info.event.title,
+                    content: info.event.extendedProps.description,
+                    trigger: 'hover',
+                    placement: 'top',
+                    container: 'body'
+                });
+            }
+        });
+
+        calendar.render();
+    }
 });
 
