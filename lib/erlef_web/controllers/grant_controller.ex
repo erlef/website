06ff294bc010f7d28e59493ef6373f53fb799d3f
Bryan Paxton <starbelly@pobox.com>
2019-08-26 17:20:57 -0500

    file and country form fixups

--- lib/erlef_web/controllers/grant_controller.ex
+++ lib/erlef_web/controllers/grant_controller.ex
@@ -3,18 +3,20 @@ defmodule ErlefWeb.GrantController do
   action_fallback ErlefWeb.FallbackController
 
   def index(conn, _params) do
-    render(conn, errors: [])
+    render(conn, errors: [], params: %{})
   end
 
   def create(%{private: %{phoenix_format: "html"}} = conn, params) do
-    case Erlef.GrantProposal.from_map(params) do
+    files = params["files"] || []
+
+    case Erlef.GrantProposal.from_map(Map.put(params, "files", files)) do
       {:ok, proposal} ->
         Erlef.GrantMail.submission(proposal) |> Erlef.Mailer.send()
         Erlef.GrantMail.submission_copy(proposal) |> Erlef.Mailer.send()
         render(conn)
 
       {:error, errs} ->
-        render(conn, "index.html", errors: errs)
+        render(conn, "index.html", params: params, errors: errs)
     end
   end
 end
--- lib/erlef_web/views/grant_view.ex
+++ lib/erlef_web/views/grant_view.ex
@@ -1,3 +1,69 @@
 defmodule ErlefWeb.GrantView do
   use ErlefWeb, :view
+
+  def grant_type_select(form) do
+    select(
+      form,
+      :grant_type,
+      [
+        [key: "--", value: "", "data-type": "none", "data-requires-coc": "false"],
+        [
+          key: "Conference",
+          value: "conference",
+          "data-type": "conference",
+          "data-requires-coc": "true"
+        ],
+        [
+          key: "Development Work",
+          value: "Development Work",
+          "data-type": "devel_work",
+          "data-requires-coc": "false"
+        ],
+        [
+          key: "Event Site Subscription",
+          value: "Event Site Subscription",
+          "data-type": "event_site_sub",
+          "data-requires-coc": "false"
+        ],
+        [
+          key: "Kids Coding Camp",
+          value: "Kids Coding Camp",
+          "data-type": "kids_camp",
+          "data-requires-coc": "false"
+        ],
+        [key: "Sprint", value: "Sprint", "data-type": "sprint", "data-requires-coc": "false"],
+        [
+          key: "Training Program",
+          value: "Training Program",
+          "data-type": "training",
+          "data-requires-coc": "false"
+        ],
+        [
+          key: "Workshop-Education",
+          value: "Workshop-Education",
+          "data-type": "workshop-edu",
+          "data-requires-coc": "true"
+        ],
+        [
+          key: "Workshop-Women",
+          value: "Workshop-Women",
+          "data-type": "workshop-women",
+          "data-requires-coc": "true"
+        ],
+        [key: "Other", value: "Other", "data-type": "other", "data-requires-coc": "false"]
+      ],
+      id: "grant_type",
+      class: "form-control"
+    )
+  end
+
+  def payment_method_select(form) do 
+    select(
+      form,
+      :payment_method,
+      [[key: "--", value: ""], "Paypal", "Zelle", "Check", "Wire"],
+      class: "form-control",
+      required: "true"
+    ) 
+  end
 end
