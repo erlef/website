f0e75530e80f01add2c089131425ae9a7a3073fb
Bryan Paxton <starbelly@pobox.com>
2019-08-26 22:20:14 -0500

    change host from nil to erlef.org for prod

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :erlef, ErlefWeb.Endpoint,
-  url: [host: nil, port: 80],
+  url: [host: "erlef.org", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
