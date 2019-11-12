d16b0df23e65872be5c7c86398f0ae5cbe2f6d32
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2019-11-11 22:29:05 -0600

    Add link to issues

--- lib/erlef/academic_papers.ex
+++ lib/erlef/academic_papers.ex
@@ -1,4 +1,8 @@
-defmodule Erlef.AcademicPaper do
+defmodule Erlef.AcademicPapers do
+  @moduledoc """
+  Context responsible for managing Academic Papers
+  """
+
   @doc """
   Using this stub for now. If we ever want to replace the datastore, we can.
   """
@@ -7,13 +11,13 @@ defmodule Erlef.AcademicPaper do
       %{
         link: "http://erlang.org/download/armstrong_thesis_2003.pdf",
         name: "Making reliable distributed systems in the presence of software errors",
-        tags: ["Erlang"],
+        tags: "Erlang",
         date: Date.new(2003, 12, 1)
       },
       %{
         link: "http://erlang.org/download/armstrong_thesis_2003.pdf",
         name: "Making reliable distributed systems in the presence of software errors",
-        tags: ["Erlang"],
+        tags: "Erlang",
         date: Date.new(2003, 12, 1)
       }
     ]
