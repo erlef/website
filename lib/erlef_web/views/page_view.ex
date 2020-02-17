d9164d5edef9e7d3f2619a539af2d096d4741911
Bryan Paxton <starbelly@pobox.com>
2020-02-17 10:35:05 -0600

    add layout partials, html helpers, and build boostrap from sass
      - added partials for twitter feed, news feed, mail subscribe, and
        working groups
      - add right_svg_hero/1 and 2 and left_svg_hero/1 and 2 ErlefWeb.HTML helpers
      - compile bootstrap from sass
      - add custom.scss file to override bootstrap variables
      - remove fullcalendar
      - refactor JsonEvents plugin to just Events
      - fix failing tests

--- lib/erlef_web/views/page_view.ex
+++ lib/erlef_web/views/page_view.ex
@@ -1,29 +1,3 @@
 defmodule ErlefWeb.PageView do
   use ErlefWeb, :view
-
-  def subscribe_form do
-    """
-     <form action="https://erlef.us20.list-manage.com/subscribe/post?u=8d8ff4d9284d463c374e574bb&amp;id=8cad7357f8" 
-           method="post" id="mc-embedded-subscribe-form" 
-           name="mc-embedded-subscribe-form" 
-           class="validate mc4wp-form mc4wp-form-116" 
-           target="_blank" novalidate> 
-         <div>
-           <div style="position: absolute; left: -5000px;" aria-hidden="true">
-             <input type="text" name="b_8d8ff4d9284d463c374e574bb_8cad7357f8" tabindex="-1" value="">
-           </div>
-
-           <div class="form-row subscribe">
-             
-               <input class="col-lg-6 form-control" type="email" name="EMAIL" placeholder="Your e-mail address" required="">
-            
-
-             
-               <input class="col-lg-auto btn btn-primary ml-2" type="submit" value="Subscribe">
-            
-           </div>
-         </div>    
-     </form>
-    """
-  end
 end
