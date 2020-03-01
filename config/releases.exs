b28480a9b6a66b3283ca2c42c1f6d291ee8f8627
Bryan Paxton <starbelly@pobox.com>
2020-03-01 16:15:29 -0600

    setup check_origin config for phoenix endpoint

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,11 @@ import Config
 config :erlef, ErlefWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  check_origin: [
+    "https://erlang.com",
+    "https://erlang.com",
+    "https://erlang.org",
+    "https://www.erlang.org",
+    "https://" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"
+  ]
