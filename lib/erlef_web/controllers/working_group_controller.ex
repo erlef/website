f1c8de567e1143919d48d67977a8e73e8a0d49a0
Bryan Paxton <bryan@starbelly.io>
2020-12-16 18:35:23 -0600

    Gracefully handle the case of no working group found
      - update show/2 to case the result of the working group query
      - redirect to index with an error message in the case of no group found

--- lib/erlef_web/controllers/working_group_controller.ex
+++ lib/erlef_web/controllers/working_group_controller.ex
@@ -6,22 +6,32 @@ defmodule ErlefWeb.WorkingGroupController do
   action_fallback ErlefWeb.FallbackController
 
   def index(conn, _params) do
-    groups =
-      Groups.all_working_groups()
-      |> Enum.reject(fn wg -> wg.slug == "eef" end)
-      |> sort_by_formation()
-
-    render(conn, working_groups: groups)
+    render(conn, working_groups: all_groups())
   end
 
   def show(conn, %{"id" => slug}) do
-    {:ok, wg} = Groups.get_working_group_by_slug(slug)
+    case Groups.get_working_group_by_slug(slug) do
+      {:ok, wg} ->
+        render(conn,
+          wg: wg,
+          blog_posts: Blogs.get_posts_by_category(slug),
+          topic: slug
+        )
 
-    render(conn,
-      wg: wg,
-      blog_posts: Blogs.get_posts_by_category(slug),
-      topic: slug
-    )
+      {:error, :not_found} ->
+        conn
+        |> put_flash(:error, "Can't seem to find that group 🤔")
+        |> redirect(to: Routes.working_group_path(conn, :index))
+
+      err ->
+        err
+    end
+  end
+
+  defp all_groups() do
+    Groups.all_working_groups()
+    |> Enum.reject(fn wg -> wg.slug == "eef" end)
+    |> sort_by_formation()
   end
 
   defp sort_by_formation(groups) do
