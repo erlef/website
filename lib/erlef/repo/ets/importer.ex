f06280106d411b3a142b127676b41621211668ee
Bryan Paxton <bryan@starbelly.io>
2020-12-28 13:30:29 -0600

    Remove custom wg importer code for wg migration
      - removed code temporarily put in place to migrate working groups to
      the database on initial deploy

--- lib/erlef/repo/ets/importer.ex
+++ lib/erlef/repo/ets/importer.ex
@@ -19,7 +19,6 @@ defmodule Erlef.Repo.ETS.Importer do
   @impl true
   def handle_continue(:compile, {}) do
     :ok = do_imports()
-    migrate_working_groups()
     repo_ref = monitor_repo()
     {:noreply, {repo_ref}}
   end
@@ -83,90 +82,6 @@ defmodule Erlef.Repo.ETS.Importer do
     }
 
     Map.merge(front, back)
-  end
-
-  alias Erlef.Groups.WorkingGroup
-  alias Erlef.Groups.Volunteer
-  alias Erlef.Groups.WorkingGroupVolunteer
-  alias Erlef.Repo
-
-  def migrate_working_groups do
-    path = "/working_groups/**/*.md"
-
-    for set <- Enum.map(find_files(path), &parse_wg_post/1) do
-      volunteers =
-        for v <- set.volunteers do
-          {maybe_insert_volunteer(v), v}
-        end
-
-      wg = maybe_insert_wg(set.working_group)
-
-      for {v, v_data} <- volunteers do
-        maybe_add_volunteer_to_wg(wg, v, v_data["is_chair"])
-      end
-    end
-  end
-
-  defp maybe_insert_wg(wg) do
-    case Repo.get_by(WorkingGroup, name: wg["name"]) do
-      %WorkingGroup{} = group ->
-        group
-
-      _ ->
-        cs = WorkingGroup.changeset(%WorkingGroup{}, wg)
-        Repo.insert!(cs)
-    end
-  end
-
-  defp maybe_insert_volunteer(v) do
-    case Repo.get_by(Volunteer, name: v["name"]) do
-      %Volunteer{} = vol ->
-        vol
-
-      _ ->
-        cs = Volunteer.changeset(%Volunteer{}, Map.put(v, "avatar_url", v["image"]))
-        Repo.insert!(cs)
-    end
-  end
-
-  defp maybe_add_volunteer_to_wg(wg, vol, is_chair) do
-    case Repo.get_by(WorkingGroupVolunteer, volunteer_id: vol.id, working_group_id: wg.id) do
-      %WorkingGroupVolunteer{} = wgv ->
-        wgv
-
-      _ ->
-        p = %{volunteer_id: vol.id, working_group_id: wg.id, is_chair: is_chair}
-        cs = WorkingGroupVolunteer.changeset(%WorkingGroupVolunteer{}, p)
-        Repo.insert!(cs)
-    end
-  end
-
-  defp parse_wg_post(file) do
-    post_str = File.read!(file)
-    trimmed_str = String.trim_leading(post_str)
-
-    case String.split(trimmed_str, ~r/[\s\r\n]---[\s\r\n]/s, parts: 3) do
-      [front, _excerpt, body] ->
-        compile_wg({front, body})
-
-      [front, body] ->
-        compile_wg({front, body})
-    end
-  end
-
-  defp compile_wg({front_str, body_str}) do
-    {:ok, data0} = Jason.decode(front_str)
-
-    back = %{
-      "proposal" => body_str
-    }
-
-    {meta, _data1} = Map.split(data0, ["email", "gcal_url", "github"])
-
-    {volunteers, wg} = Map.split(data0, ["volunteers"])
-
-    wg = Map.put(Map.merge(wg, back), "meta", meta)
-    %{working_group: wg, volunteers: volunteers["volunteers"]}
   end
 
   defp find_files(wildcard) do
