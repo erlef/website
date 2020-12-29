56101cf79895dc8312e7dca49b2d5f79b5e65475
Bryan Paxton <bryan@starbelly.io>
2020-12-29 13:59:45 -0600

    Recreate the database every time on test
     - mix test was not dropping and creating the database affording
     opportunity for failing tests not to be caught
     - fixup working group related tests

--- test/erlef_web/controllers/working_group_controller_test.exs
+++ test/erlef_web/controllers/working_group_controller_test.exs
@@ -1,13 +1,20 @@
 defmodule ErlefWeb.WorkingGroupControllerTest do
   use ErlefWeb.ConnCase
 
+  setup do
+    wgv = insert(:working_group_volunteer)
+    insert(:working_group_chair, volunteer: wgv.volunteer, working_group: wgv.working_group)
+    [working_group: wgv.working_group]
+  end
+
   test "GET /wg", %{conn: conn} do
     conn = get(conn, Routes.working_group_path(conn, :index))
     assert html_response(conn, 200) =~ "the EEF sponsors Working"
   end
 
-  test "GET /wg/:id", %{conn: conn} do
-    conn = get(conn, Routes.working_group_path(conn, :show, "marketing"))
-    assert html_response(conn, 200) =~ "Marketing Working Group"
+  test "GET /wg/:id", %{conn: conn, working_group: wg} do
+    conn = get(conn, Routes.working_group_path(conn, :show, wg.slug))
+    name = wg.name
+    assert html_response(conn, 200) =~ name
   end
 end
