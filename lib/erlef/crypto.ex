bc768a6cc33a11ba99db69673d6bef3d4a2d1a27
Bryan Paxton <bryan@starbelly.io>
2021-01-30 00:23:59 -0600

    Add simple tests and clean up Erlef.Crypto

--- lib/erlef/crypto.ex
+++ lib/erlef/crypto.ex
@@ -1,8 +1,21 @@
 defmodule Erlef.Crypto do
-  @moduledoc false
+  @moduledoc """
+  Erlef.Crypto
 
+  Provides wrappers around common cryptographic operations and dependencies
+  """
+
+  @doc """
+  Compares two strings using `Plug.Crypto.secure_compare/2` to mitigate timing attacks.
+  """
+  @spec compare(String.t(), String.t()) :: boolean()
   def compare(str1, str2), do: Plug.Crypto.secure_compare(str1, str2)
 
+  @doc """
+  Computes Message Authencation Code (MAC) via `:erlang.mac/4`. 
+  The computed MAC is base 16 encoded and lower cased. 
+  """
+  @spec hmac_encode(binary(), binary()) :: String.t()
   def hmac_encode(key, data) do
     :hmac
     |> :crypto.mac(:sha3_256, key, data)
