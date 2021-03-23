4fed147abfcbf3b0f2603d7fada097bf54ca70ae
Bryan Paxton <39971740+starbelly@users.noreply.github.com>
2021-03-22 22:52:23 -0500

    Log errors at warning level instead of error (#299)

--- lib/erlef/agenda.ex
+++ lib/erlef/agenda.ex
@@ -75,7 +75,7 @@ defmodule Erlef.Agenda do
         err ->
           # credo:disable-for-lines:3
           if Erlef.in_env?([:dev, :prod]) do
-            Logger.error(fn -> "Error getting feed #{l} : #{inspect(err)}" end)
+            Logger.warning(fn -> "Error getting feed #{l} : #{inspect(err)}" end)
           end
 
           acc
