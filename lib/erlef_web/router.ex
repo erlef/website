0a2878fce6c78f3982a8054d6fa8b82970534779
Bryan Paxton <39971740+starbelly@users.noreply.github.com>
2021-04-08 13:51:03 -0500

    Switch to Montserrat (#327)
    This PR changes us from using canada-gibson font which is proprietary and presents accessibility issues on certain platforms and browsers.

--- lib/erlef_web/router.ex
+++ lib/erlef_web/router.ex
@@ -11,7 +11,7 @@ defmodule ErlefWeb.Router do
     avatar-images.ewr1.vultrobjects.com event-org-images.ewr1.vultrobjects.com 
     sponsors.ewr1.vultrobjects.com erlef.matomo.cloud cdn.rawgit.com
     127.0.0.1:9998 cdn.datatables.net user.fm cdnjs.cloudflare.com uicdn.toast.com
-    plausible.io
+    plausible.io fonts.googleapis.com fonts.gstatic.com
   )
 
   @trusted_connect_sources ~w(https://user.fm https://www.erlef.org wss://erlef.org 
