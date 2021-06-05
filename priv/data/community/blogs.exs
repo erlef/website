6962363fca6801b2174bb1cd70199819c67da1e4
Juha Lehtonen <hello@juha.tl>
2021-06-05 11:35:56 +0300

    287: Add Blogs section to Community page

---
+++ priv/data/community/blogs.exs
@@ -0,0 +1,82 @@
+[
+  %{
+    name: "Dashbit",
+    link: "https://dashbit.co/blog/",
+    icon: "/images/community/blogs/dashbit.png",
+    logo: nil,
+    about:
+      "Dashbit copy."
+  },
+  %{
+    name: "OTP Team",
+    link: "https://blog.erlang.org/",
+    icon: "/images/community/blogs/otp.png",
+    logo: nil,
+    about:
+      "The Erlang/OTP team at Ericsson, the implementors and maintainers of Erlang/OTP."
+  },
+  %{
+    name: "Elixir Lang",
+    link: "https://elixir-lang.org/blog/",
+    icon: "/images/community/blogs/elixir.png",
+    logo: nil,
+    about:
+      "Official blog of the Elixir programming language."
+  },
+  %{
+    name: "Gleam News",
+    link: "https://gleam.run/news/",
+    icon: "/images/community/languages/gleam.svg",
+    logo: nil,
+    about:
+      "Latest official news on the Gleam programming language."
+  },
+  %{
+    name: "LFE blog",
+    link: "https://blog.lfe.io/",
+    icon: "/images/community/blogs/lfe.png",
+    logo: nil,
+    about:
+      "Official blog of the LFE programming language."
+  },
+  %{
+    name: "Planet Erlang",
+    link: "http://planeterlang.com/",
+    icon: nil,
+    logo: nil,
+    about:
+      "Planet Erlang is a meta blog that collects posts from the blogs of various Erlang developers and contributors."
+  },
+  %{
+    name: "Elixir Radar",
+    link: "https://elixir-radar.com/",
+    icon: nil,
+    logo: nil,
+    about:
+      "Keep updated with what's happening in the Elixir community."
+  },
+  %{
+    name: "Changelog",
+    link: "https://changelog.com/topic/elixir/",
+    icon: "/images/community/blogs/changelog.png",
+    logo: nil,
+    about:
+      "Changelog"
+  },
+  %{
+    name: "Elixir Status",
+    link: "https://elixirstatus.com/",
+    icon: "/images/community/blogs/elixirstatus.png",
+    logo: nil,
+    about:
+      "Elixir Status"
+  },
+  %{
+    name: "Elixir Weekly",
+    link: "https://elixirweekly.net/",
+    icon: "/images/community/blogs/elixirweekly.png",
+    logo: nil,
+    about:
+      "Elixir Weekly"
+  },
+]
