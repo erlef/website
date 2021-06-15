85e3573a2b47bc8afe094ef76bafb49b6af923eb
Juha <juhalehtonen@users.noreply.github.com>
2021-06-15 21:58:11 +0300

    287: Add Blogs section to Community page (#342)
    * 287: Add Blogs section to Community page
    
    Co-authored-by: José Valim <jose.valim@gmail.com>
    Co-authored-by: Bryan Paxton <39971740+starbelly@users.noreply.github.com>

---
+++ priv/data/community/blogs.exs
@@ -0,0 +1,82 @@
+[
+  %{
+    name: "Dashbit",
+    link: "https://dashbit.co/blog/",
+    icon: "/images/community/blogs/dashbit.png",
+    logo: nil,
+    about:
+      "Maintainers and contributors to many Elixir projects, such as Ecto, Broadway, LiveView, and Elixir itself."
+  },
+  %{
+    name: "OTP Team",
+    link: "https://blog.erlang.org/",
+    icon: "/images/community/blogs/otp.png",
+    logo: nil,
+    about:
+      "The Erlang/OTP team at Ericsson, the implementors and maintainers of Erlang/OTP."
+  },
+  %{
+    name: "Elixir Lang",
+    link: "https://elixir-lang.org/blog/",
+    icon: "/images/community/blogs/elixir.png",
+    logo: nil,
+    about:
+      "Official blog of the Elixir programming language."
+  },
+  %{
+    name: "Gleam News",
+    link: "https://gleam.run/news/",
+    icon: "/images/community/languages/gleam.svg",
+    logo: nil,
+    about:
+      "Latest official news on the Gleam programming language."
+  },
+  %{
+    name: "LFE blog",
+    link: "https://blog.lfe.io/",
+    icon: "/images/community/blogs/lfe.png",
+    logo: nil,
+    about:
+      "Official blog of the LFE programming language."
+  },
+  %{
+    name: "Planet Erlang",
+    link: "http://planeterlang.com/",
+    icon: nil,
+    logo: nil,
+    about:
+      "Planet Erlang is a meta blog that collects posts from the blogs of various Erlang developers and contributors."
+  },
+  %{
+    name: "Elixir Radar",
+    link: "https://elixir-radar.com/",
+    icon: nil,
+    logo: nil,
+    about:
+      "Elixir Radar is a weekly Elixir newsletter with curated content and a job board for Elixir developers."
+  },
+  %{
+    name: "Changelog",
+    link: "https://changelog.com/topic/elixir/",
+    icon: "/images/community/blogs/changelog.png",
+    logo: nil,
+    about:
+      "Changelog is an indie media company creating content for software developers, including a dedicated topic for Elixir."
+  },
+  %{
+    name: "ElixirStatus",
+    link: "https://elixirstatus.com/",
+    icon: "/images/community/blogs/elixirstatus.png",
+    logo: nil,
+    about:
+      "ElixirStatus is a community where Elixir creators post their newest blog posts, project updates, podcast episodes and videos."
+  },
+  %{
+    name: "ElixirWeekly",
+    link: "https://elixirweekly.net/",
+    icon: "/images/community/blogs/elixirweekly.png",
+    logo: nil,
+    about:
+      "ElixirWeekly is focusing on the very latest creations of the community in one weekly email."
+  },
+]
