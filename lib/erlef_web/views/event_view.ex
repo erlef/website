e5d643329c23e45662534d004e26f4a34ca0b50d
Bryan Paxton <bryan@starbelly.io>
2021-06-26 14:10:33 -0500

    Add default icon for events
     This commit adds a new default icon thanks to @geraborras of the EEF marcom team.
    
     - Add new default event icon
     - Add view helpers to ensure we use the default event icon and color when the organizer brand logo or color is nil
     - Use said view helpers in the events live index page
     - Likewise use the new view helpers within the main index page

--- lib/erlef_web/views/event_view.ex
+++ lib/erlef_web/views/event_view.ex
@@ -1,9 +1,10 @@
 defmodule ErlefWeb.EventView do
   use ErlefWeb, :view
 
-  import ErlefWeb.ViewHelpers, only: [event_dates: 2]
+  import ErlefWeb.ViewHelpers
 
   @spec active_class(String.t(), String.t()) :: String.t()
   def active_class(selected, link_type) when selected == link_type, do: "active"
   def active_class(_selected, _link_type), do: ""
+
 end
