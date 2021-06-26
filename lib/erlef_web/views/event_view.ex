97828acfdb157f1338f4ab2a65a18bd853112e7d
Bryan Paxton <bryan@starbelly.io>
2021-06-26 14:21:58 -0500

    format

--- lib/erlef_web/views/event_view.ex
+++ lib/erlef_web/views/event_view.ex
@@ -6,5 +6,4 @@ defmodule ErlefWeb.EventView do
   @spec active_class(String.t(), String.t()) :: String.t()
   def active_class(selected, link_type) when selected == link_type, do: "active"
   def active_class(_selected, _link_type), do: ""
-
 end
--- lib/erlef_web/views/view_helpers.ex
+++ lib/erlef_web/views/view_helpers.ex
@@ -108,7 +108,6 @@ defmodule ErlefWeb.ViewHelpers do
 
   def image_path(conn, path), do: ErlefWeb.Router.Helpers.static_path(conn, path)
 
-
   def event_organizer_logo(nil), do: "/images/default_event_icon.svg"
   def event_organizer_logo(logo_path), do: logo_path
 
