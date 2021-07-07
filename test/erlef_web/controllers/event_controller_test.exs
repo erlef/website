f63b99aaea776d11e12067462d1f85725668d193
Bryan Paxton <bryan@starbelly.io>
2021-07-06 19:12:26 -0500

    Remove slug support on Erlef.Community.Event
    We don't currently utilize slugs on events at this point and slugs end up blocking certain recurring events
    from being added without coming up with a clever title in order to generate a unique slug. If for some reason
    this changes in the future, we can simply add it back.

--- test/erlef_web/controllers/event_controller_test.exs
+++ test/erlef_web/controllers/event_controller_test.exs
@@ -7,22 +7,4 @@ defmodule ErlefWeb.EventControllerTest do
     assert html_response(conn, 200) =~
              "The EEF participates in many events related to the BEAM Community."
   end
-
-  test "GET /event/:id", %{conn: conn} do
-    member = insert_member!("basic_member")
-    admin = insert_member!("admin")
-
-    e =
-      insert!(:event,
-        type: :meetup,
-        approved: true,
-        submitted_by_id: member.id,
-        approved_by_id: admin.id,
-        slug: "eh"
-      )
-
-    conn = get(conn, Routes.event_path(conn, :show, e.id))
-
-    assert html_response(conn, 200)
-  end
 end
