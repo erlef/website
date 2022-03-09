615a0e498a89e9d76a8846d2b3a713299c6081a4
Bryan Paxton <bryan@starbelly.io>
2022-03-08 22:03:07 -0600

    fixup flaky tests

--- test/erlef_web/views/page_view_test.exs
+++ test/erlef_web/views/page_view_test.exs
@@ -5,17 +5,17 @@ defmodule ErlefWeb.PageViewTest do
 
   test "community_card_wall/1" do
     items = [%{icon: nil, logo: "logo", name: "name", about: "about", link: "link"}]
-    assert {:safe, _html} = PageView.community_card_wall(items)
+    assert %Phoenix.LiveView.Rendered{} = PageView.community_card_wall(items)
 
     items = [%{icon: "icon", logo: nil, name: "name", about: "about", link: "link"}]
-    assert {:safe, _html} = PageView.community_card_wall(items)
+    assert %Phoenix.LiveView.Rendered{} = PageView.community_card_wall(items)
 
     items = [%{icon: nil, logo: nil, name: "name", about: "about", link: "link"}]
-    assert {:safe, _html} = PageView.community_card_wall(items)
+    assert %Phoenix.LiveView.Rendered{} = PageView.community_card_wall(items)
   end
 
   test "fellows_card_wall/1" do
     fellows = [%{name: "name", avatar: "avatar", about: "about"}]
-    assert {:safe, _html} = PageView.fellows_card_wall(fellows)
+    assert %Phoenix.LiveView.Rendered{} = PageView.fellows_card_wall(fellows)
   end
 end
