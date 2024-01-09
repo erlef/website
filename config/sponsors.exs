49c73b96a4f1a649f3652ed41f8f4bb315bd8606
Bryan Paxton <bryan@starbelly.io>
2024-01-08 22:53:55 -0600

    Revert to listing sponsors from static files per vultr outage

---
+++ config/sponsors.exs
@@ -0,0 +1,287 @@
+use Mix.Config
+
+config :erlef, :sponsors, [
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/enki-logo.png",
+    name: "Enki multimedia",
+    url: "https://enki-multimedia.org/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: true,
+    logo: nil,
+    logo_url: "/images/sponsors/crevalle.png",
+    name: "Crevalle",
+    url: "https://crevalle.io/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/arcblock-logo.png",
+    name: "ArcBlock",
+    url: "https://www.arcblock.io/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/aeternityae-logo.png",
+    name: "Aeternity Foundation",
+    url: "https://www.aeternity-foundation.org/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: true,
+    logo: nil,
+    logo_url: "/images/sponsors/adroll-logo.png",
+    name: "AdRoll",
+    url: "https://www.adroll.com/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/dockyard-logo.png",
+    name: "Dockyard",
+    url: "https://dockyard.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/84codes-logo.png",
+    name: "84codes",
+    url: "https://www.84codes.com"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/avassa-logo.png",
+    name: "Avassa",
+    url: "https://avassa.io/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/hca-logo.png",
+    name: "HCA",
+    url: "https://hcahealthcare.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/weedmaps-logo.png",
+    name: "Weedmaps",
+    url: "https://weedmaps.com/careers"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/plausible-logo.png",
+    name: "Plausible",
+    url: "https://plausible.io/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/alembic-logo.png",
+    name: "Alembic ",
+    url: "https://alembic.com.au/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/learn-elixir-logo.png",
+    name: "Learn Elixir",
+    url: "https://learn-elixir.dev/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/dnsimple-logo.png",
+    name: "dnsimple",
+    url: "https://dnsimple.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/binary_noggin-logo.png",
+    name: "Binary Noggin",
+    url: "https://binarynoggin.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/cisco-logo.png",
+    name: "Cisco",
+    url: "https://www.cisco.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/coders51-logo.png",
+    name: "coders51",
+    url: "https://www.coders51.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/ericsson-logo.png",
+    name: "Ericsson",
+    url: "https://www.ericsson.com/en"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/simplifi-logo.png",
+    name: "simplifi",
+    url: "https://simpli.fi/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/esl-logo.png",
+    name: "Erlang Solutions",
+    url: "https://www.erlang-solutions.com/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/genetec-logo.png",
+    name: "Genetec",
+    url: "https://www.genetec.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/happi-hacking-logo.png",
+    name: "happihacking",
+    url: "http://happihacking.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/id3as-logo.png",
+    name: "id3as",
+    url: "https://www.id3as.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/ninefx-logo.png",
+    name: "NineFX",
+    url: "https://www.ninefx.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/otolo-logo.png",
+    name: "oTolo Networks",
+    url: "https://www.otolonetworks.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/pharos-logo.png",
+    name: "Pharos",
+    url: "https://www.pharos-corp.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/sfractal-logo.png",
+    name: "sFractal Consulting LLC",
+    url: "https://www.sfractal.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/whatsapp-logo.png",
+    name: "WhatsApp",
+    url: "https://www.whatsapp.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/peer-logo.png",
+    name: "Peer Stritzinger GmbH",
+    url: "https://www.stritzinger.com/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: true,
+    logo_url: "/images/klarna-logo.png",
+    name: "Klarna",
+    url: "https://www.klarna.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/dashbit-logo.png",
+    name: "Dashbit",
+    url: "https://dashbit.co/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/shiguredo-logo.png",
+    name: "Shiguredo",
+    url: "https://shiguredo.jp"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/ispirata-logo.png",
+    name: "Ispirata",
+    url: "https://www.ispirata.com/"
+  },
+  %{
+    active: false,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/pepsico-logo.png",
+    name: "PepsiCo eCommerce ",
+    url: "https://pepsi.co/ecommerce"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/spiff-logo.png",
+    name: "Spiff",
+    url: "https://spiff.com"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/tiot-logo.png",
+    name: "TI Tokyo",
+    url: "https://www.tiot.jp/en/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/emq-logo.png",
+    name: "EMQ",
+    url: "https://www.emqx.io/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/revelry-logo.png",
+    name: "Revelry",
+    url: "https://revelry.co/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: true,
+    logo_url: "/images/sponsors/mainmatter-logo.png",
+    name: "Mainmatter",
+    url: "https://mainmatter.com/"
+  },
+  %{
+    active: true,
+    is_founding_sponsor: false,
+    logo_url: "/images/sponsors/smartrent-logo.png",
+    name: "SmartRent",
+    url: "https://smartrent.com/"
+  }
+]
+
