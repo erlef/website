dcbbeea3b12d77a5938975a3a7b987d6a859077f
Ben Marx <ben@bgmarx.com>
2024-03-28 20:52:53 -0700

    Add the initial 2024 board election blog post (#473)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 Application.ensure_started(Erlef.WildApricot.Cache)
 Application.ensure_started(Erlef.Test.S3)
-ExUnit.start()
+ExUnit.start(exclude: [:pending])
 Faker.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(Erlef.Repo, {:shared, self()})
