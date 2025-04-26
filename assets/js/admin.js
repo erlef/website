9a852e7196a3f1d956f640e40c70bc55fa3b8a3f
Jonatan Männchen <jonatan@maennchen.ch>
2025-04-26 19:06:36 +0000

    Bootstrap5 Update

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,20 +1,17 @@
-import css from "../css/admin.scss"
-import "phoenix_html"
-import "jquery"
-import 'bootstrap'
-import 'admin-lte'
-import 'popper.js'
+import css from "../css/admin.scss";
+import "phoenix_html";
+import "jquery";
+import "bootstrap";
+import "admin-lte";
 
-$('[data-toggle="tooltip"]').tooltip();
-
+// $('[data-bs-toggle="tooltip"]').tooltip();
 
 export default class App {
   constructor() {
-    
-    $(document).ready( function () {
-        $("[data-toggle='popover']").popover({container: "body", html: true, animation: false })
+    $(document).ready(function () {
+      $("[data-bs-toggle='popover']").popover({ container: "body", html: true, animation: false });
     });
   }
 }
 
-window.app = new App()
+window.app = new App();
