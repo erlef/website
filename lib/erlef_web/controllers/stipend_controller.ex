fe05e2e95491226d86444ec9a8b6acc9ad8c1dfc
Jonatan Männchen <jonatan@maennchen.ch>
2025-12-21 13:32:43 +0100

    Replace Stipend form with External Form

--- lib/erlef_web/controllers/stipend_controller.ex
+++ lib/erlef_web/controllers/stipend_controller.ex
@@ -1,34 +1,15 @@
 defmodule ErlefWeb.StipendController do
   use ErlefWeb, :controller
-  action_fallback ErlefWeb.FallbackController
 
   def index(conn, _params) do
-    render(conn, errors: [], params: %{})
+    render(conn)
   end
 
   def faq(conn, _params) do
-    render(conn, errors: [], params: %{})
-  end
-
-  def form(conn, %{"type" => type}) do
-    render(conn, errors: [], params: %{}, type: type)
+    render(conn)
   end
 
   def form(conn, _params) do
-    render(conn, errors: [], params: %{}, type: "default")
-  end
-
-  def create(%{private: %{phoenix_format: "html"}} = conn, params) do
-    files = params["files"] || []
-
-    case Erlef.StipendProposal.from_map(Map.put(params, "files", files)) do
-      {:ok, proposal} ->
-        Erlef.StipendMail.submission(proposal) |> Erlef.Mailer.send()
-        Erlef.StipendMail.submission_copy(proposal) |> Erlef.Mailer.send()
-        render(conn)
-
-      {:error, errs} ->
-        render(conn, "form.html", type: params["type"], params: params, errors: errs)
-    end
+    render(conn)
   end
 end
